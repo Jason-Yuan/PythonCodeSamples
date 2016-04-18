09db903685bf681eda51ad6d27a2e7411ebe4e25
Jason-Yuan <yuanbo0109@gmail.com>
2016-04-17 23:43:12 -0700

    update convert file

--- Fixing Mojibake in CUE file/Convert.py
+++ Fixing Mojibake in CUE file/Convert.py
@@ -2,19 +2,19 @@ import os, glob
 import chardet
 from codecs import BOM_UTF8
 
-os.chdir("/Users/boyuan/Desktop/Jimmy")
+os.chdir("/Path/to/your/folder")
+
+# grap all *.cue files in target folder
 for file in glob.glob("*.cue"):
 	with open(file) as f:
 		content = f.read()
 		code = chardet.detect(content)['encoding']
-		print code
 		if code == 'UTF-8':
 			continue
 		try:
 			content = content.decode(code).encode('utf8')
-			print content
 		except:
-			print 'cannot encode', file
+			print 'Unable to encode', file
 			continue
 		new_cue = os.path.splitext(file)[0]+'-UTF8.cue'
 		with open(new_cue, 'w') as f:
