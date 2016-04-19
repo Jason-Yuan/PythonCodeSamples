aa76cf57360efaa5733e508a2bae198ef272a46f
Jason-Yuan <yuanbo0109@gmail.com>
2016-04-18 23:08:17 -0700

    enable traversal all sub folders

--- Fixing Mojibake in CUE file/Convert.py
+++ Fixing Mojibake in CUE file/Convert.py
@@ -2,21 +2,23 @@ import os, glob
 import chardet
 from codecs import BOM_UTF8
 
-os.chdir("/Path/to/your/folder")
+rootDir = "/Your/Root/Dir/Path"
 
-# grap all *.cue files in target folder
-for file in glob.glob("*.cue"):
-	with open(file) as f:
-		content = f.read()
-		code = chardet.detect(content)['encoding']
-		if code == 'UTF-8':
-			continue
-		try:
-			content = content.decode(code).encode('utf8')
-		except:
-			print 'Unable to encode', file
-			continue
-		new_cue = os.path.splitext(file)[0]+'-UTF8.cue'
-		with open(new_cue, 'w') as f:
-			f.write(BOM_UTF8)
-			f.write(content)
+for dirName, subdirList, fileList in os.walk(rootDir):
+	# print('Found directory: %s' % dirName)
+	os.chdir(dirName)
+	for file in glob.glob("*.cue"):
+		with open(file) as f:
+			content = f.read()
+			code = chardet.detect(content)['encoding']
+			if code == 'UTF-8':
+				continue
+			try:
+				content = content.decode(code).encode('utf8')
+			except:
+				print 'Unable to encode', file
+				continue
+			new_cue = os.path.splitext(file)[0]+'-UTF8.cue'
+			with open(new_cue, 'w') as f:
+				f.write(BOM_UTF8)
+				f.write(content)
